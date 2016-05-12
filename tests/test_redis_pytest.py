3242ae8d0c637612bf5b929f370d3ec01dcb7535
Samy Abidib <samy.abidib@shopify.com>
2016-05-11 23:43:05 -0400

    Added test checking ensuring external arguments are not affected by plugin

--- tests/test_redis_pytest.py
+++ tests/test_redis_pytest.py
@@ -4,6 +4,8 @@ Tests should be launched from the root directory with:
 py.test --redis-port=<port> --redis-host=<host> --redis-list-key=<list_to_use>
 
 """
+# Exit value when everything is okay
+from _pytest.main import EXIT_OK
 # Exit value when no tests are collected
 from _pytest.main import EXIT_NOTESTSCOLLECTED
 # Exit value whenever a single test fails
@@ -21,6 +23,28 @@ def get_option_array(option_dict):
     """Return cmdline options for a dict in '--key=val' form."""
     return ["--{}={}".format(k, v) for k, v in option_dict.items()]
 
+
+def test_external_arguments(testdir, redis_connection, redis_args):
+    """Ensure that the plugin doesn't intefere with other plugins."""
+    import os.path
+    tmp_test_filename = "test_external_arguments.py"
+    testdir.makepyfile("""
+        def test_run_should_run():
+            assert True
+    """)
+    redis_connection.lpush(redis_args['redis-list-key'],
+                           tmp_test_filename + "::test_run_should_run")
+
+    junitxml_filename = "pytest.xml"
+    py_test_args = default_pytest_redis_args() + \
+        get_option_array(redis_args) + \
+        ['--junitxml=' + junitxml_filename]
+
+    junitxml_path = str(testdir.tmpdir) + "/" + junitxml_filename
+
+    result = testdir.runpytest(*py_test_args)
+    assert os.path.exists(junitxml_path)
+    assert result.ret == EXIT_OK
 
 def test_no_consumption_of_item(testdir, redis_args):
     """Make sure that we don't run tests when the list is empty."""
