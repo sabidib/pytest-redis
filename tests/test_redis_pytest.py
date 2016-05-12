1bdb2fe3fb389f656fdde5f981450c82ac2993f1
Samy Abidib <samy.abidib@shopify.com>
2016-05-11 22:40:23 -0400

    Updated tests

--- tests/test_redis_pytest.py
+++ tests/test_redis_pytest.py
@@ -1,145 +1,77 @@
 """Tests the pytest-redis with a running redis instance.
 
 Tests should be launched from the root directory with:
-py.test  --redis-port=<port> --redis-host=<host> --redis-list-key=<list_to_use>
+py.test --redis-port=<port> --redis-host=<host> --redis-list-key=<list_to_use>
 
 """
+# Exit value when no tests are collected
+from _pytest.main import EXIT_NOTESTSCOLLECTED
+# Exit value whenever a single test fails
+from _pytest.main import EXIT_TESTSFAILED
+# Exit value when an exception is thrown by parser or KeyboardInterrupt
+from _pytest.main import EXIT_INTERRUPTED
 
 
-def test_consumption_of_item(testdir, redis_args, redis_connection):
-    """Make sure that we can consume multiple items."""
-    [redis_connection.lpush(redis_args['redis-list-key'],
-                            "test_consumption_of_item.py::test_run" + str(i))
-        for i in range(5)]
+def default_pytest_redis_args():
+    """Return default options for each pytest execution."""
+    return ['-v', '-p', 'pytest_redis']
 
-    # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_run0():
-            assert True
 
-        def test_run1():
-            assert True
-
-        def test_run2():
-            assert True
-
-        def test_run3():
-            assert True
-
-        def test_run4():
-            assert True
-
-        def test_run5():
-            assert True
-    """)
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '-v',
-        '-p',
-        'pytest_redis',
-        *["--" + str(key) + "=" + str(val) for key, val in redis_args.items()]
-    )
-
-    result.stdout.fnmatch_lines_random([
-        "*::test_run4 PASSED",
-        "*::test_run3 PASSED",
-        "*::test_run2 PASSED",
-        "*::test_run1 PASSED",
-        "*::test_run0 PASSED"
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
+def get_option_array(option_dict):
+    """Return cmdline options for a dict in '--key=val' form."""
+    return ["--{}={}".format(k, v) for k, v in option_dict.items()]
 
 
 def test_no_consumption_of_item(testdir, redis_args):
-    """Make sure that we can consume multiple items."""
-    # create a temporary pytest test module
+    """Make sure that we don't run tests when the list is empty."""
     testdir.makepyfile("""
         def test_run_should_run():
             assert True
     """)
+    py_test_args = default_pytest_redis_args() + get_option_array(redis_args)
 
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '-v',
-        '-p',
-        'pytest_redis',
-        *["--" + str(key) + "=" + str(val) for key, val in redis_args.items()]
-    )
-
-    result.stdout.fnmatch_lines_random([
-        "*No items in redis list*"
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 5
+    result = testdir.runpytest(*py_test_args)
+    assert result.ret == EXIT_NOTESTSCOLLECTED
 
 
-def test_rpop_from_queue(testdir, redis_connection, redis_args):
-    """Specify rpop from redis queue with --redis-pop-type=rpop."""
-    [redis_connection.lpush(redis_args['redis-list-key'],
-                            "test_rpop_from_queue.py::test_run" + str(i))
-        for i in range(2)]
-    # create a temporary pytest test module
+def test_lr_pop_from_list(testdir, redis_connection, redis_args):
+    """Specify rpop from redis list with --redis-pop-type=rpop."""
+    tmp_test_filename = "test_lr_pop_from_list.py"
+
     testdir.makepyfile("""
         def test_run0():
-            assert True
+            assert False
 
         def test_run1():
             assert True
     """)
 
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '-v',
-        '-p',
-        'pytest_redis',
-        '--redis-pop-type=rpop',
-        *["--" + str(key) + "=" + str(val) for key, val in redis_args.items()]
-    )
+    py_test_args = default_pytest_redis_args() + get_option_array(redis_args)
 
-    # The order of the line match indicates if it was rpop-ed correctly
-    result.stdout.fnmatch_lines([
-        "*::test_run0 PASSED",
-        "*::test_run1 PASSED"
-    ])
+    pop_options = ['rpop', 'lpop', 'invalid']
 
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
+    for pop_dir in pop_options:
+        cur_args = py_test_args + ["--redis-pop-type=" + pop_dir]
+        # populate redis list with tests
+        for ind in range(2):
+            redis_connection.lpush(redis_args['redis-list-key'],
+                                   tmp_test_filename + "::test_run" + str(ind))
 
-
-def test_lpop_from_queue(testdir, redis_connection, redis_args):
-    """Specify lpop from redis queue with --redis-pop-type=lpop."""
-    [redis_connection.lpush(redis_args['redis-list-key'],
-                            "test_lpop_from_queue.py::test_run" + str(i))
-        for i in range(2)]
-    # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_run0():
-            assert True
-
-        def test_run1():
-            assert True
-    """)
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '-v',
-        '-p',
-        'pytest_redis',
-        '--redis-pop-type=lpop',
-        *["--" + str(key) + "=" + str(val) for key, val in redis_args.items()]
-    )
-    # The order of the line match indicates if it was lpop-ed correctly
-    result.stdout.fnmatch_lines([
-        "*::test_run1 PASSED",
-        "*::test_run0 PASSED"
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
-
-
-
+        result = testdir.runpytest(*cur_args)
+        if pop_dir == 'rpop':
+            result.stdout.fnmatch_lines([
+                "*::test_run0 FAILED",
+                "*::test_run1 PASSED"
+            ])
+            assert result.ret == EXIT_TESTSFAILED
+        elif pop_dir == 'lpop':
+            result.stdout.fnmatch_lines([
+                "*::test_run1 PASSED",
+                "*::test_run0 FAILED"
+            ])
+            assert result.ret == EXIT_TESTSFAILED
+        elif pop_dir == 'invalid':
+            # Clean up redis list because we have an invalid cmd line opt
+            redis_connection.rpop(redis_args['redis-list-key'])
+            redis_connection.rpop(redis_args['redis-list-key'])
+            assert result.ret == EXIT_INTERRUPTED
