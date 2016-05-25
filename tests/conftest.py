6189f0693f8dbd375ef6d67d60dc0522c4b363fd
Samy Abidib <samy.abidib@shopify.com>
2016-05-24 21:49:40 -0400

    Updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,25 @@ def pytest_addoption(parser):
                      default=False)
 
 
+def handle_existing_list(redis_connection, do_force_deletion, list_key):
+    """Clear a list in a redis instance or warns the user about deletion."""
+    if redis_connection.llen(list_key):
+        if not do_force_deletion:
+            exit_string = ("redis list '{}' exists in redis instance. "
+                           "pass --force to ignore this and delete "
+                           "list.").format(list_key)
+            pytest.exit(exit_string)
+        else:
+            print "Deleting pre-exiting redis list '{}'".format(
+                list_key)
+            clean_list(redis_connection, list_key)
+
+
+def clean_list(redis_connection, list_key):
+    """Delete a list in the given redis instance."""
+    return redis_connection.delete(list_key)
+
+
 @pytest.fixture
 def redis_host(request):
     """Return redis_host cmdline arg."""
@@ -36,35 +55,39 @@ def redis_list_key(request):
 
 
 @pytest.fixture
+def redis_backup_list_key(request):
+    """Return redis_list_key cmdline arg."""
+    return request.config.getoption("--redis-backup-list-key")
+
+
+@pytest.fixture
 def force_del_lists(request):
     """Return force_del_lists cmdline arg."""
     return request.config.getoption("--force")
 
 
 @pytest.fixture
-def redis_args(redis_host, redis_port, redis_list_key):
+def redis_args(redis_host, redis_port, redis_list_key,
+               redis_backup_list_key):
     """Return cmdline args for redis."""
     return {
         'redis-host': redis_host,
         'redis-port': redis_port,
-        'redis-list-key': redis_list_key
+        'redis-list-key': redis_list_key,
+        'redis-backup-list-key': redis_backup_list_key
     }
 
 
-@pytest.fixture
+@pytest.yield_fixture
 def redis_connection(redis_args, force_del_lists):
     """Return redis_connection and check for empty redist-list-key."""
     r_client = redis.StrictRedis(host=redis_args['redis-host'],
                                  port=redis_args['redis-port'])
-    if r_client.llen(redis_args['redis-list-key']):
-        if not force_del_lists:
-            exit_string = ("redis list '{}' exists in redis instance. "
-                           "pass --force to ignore this and delete "
-                           "list.").format(redis_args['redis-list-key'])
-            pytest.exit(exit_string)
-        else:
-            print "Deleting pre-exiting redis list '{}'".format(
-                redis_args['redis-list-key'])
-            r_client.delete(redis_args['redis-list-key'])
+    handle_existing_list(r_client, force_del_lists,
+                         redis_args["redis-list-key"])
+    handle_existing_list(r_client, force_del_lists,
+                         redis_args["redis-backup-list-key"])
 
-    return r_client
+    yield r_client
+    clean_list(r_client, redis_args["redis-backup-list-key"])
+    clean_list(r_client, redis_args["redis-list-key"])
